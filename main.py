52483205f6cd939daa577642c1e282d0b47643a0
Shivam Panigrahi <73484687+shivam821@users.noreply.github.com>
2024-02-25 18:47:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,26 @@
 import os
 import shutil
 
-file_path = input("Enter the path : ")
+# Get the input from the user for the directory path
+file_path = input("Enter the path: ")
 
+# List all files in the specified directory
 files = os.listdir(file_path)
 
-for i in files : 
-    filename,extention = os.path.splitext(i)
-    extention = extention[1:]
+# Iterate through each file in the directory
+for i in files:
+    # Split the filename and extension
+    filename, extension = os.path.splitext(i)
+    extension = extension[1:]  # Remove the leading dot from the extension
 
-    if os.path.exists(file_path + '/' + extention) : 
-        shutil.move(file_path + '/' + i, file_path + '/' + extention + '/' + i)
+    # Check if a directory with the same extension exists
+    if os.path.exists(file_path + '/' + extension):
+        # Move the file to the directory with the corresponding extension
+        shutil.move(file_path + '/' + i, file_path + '/' + extension + '/' + i)
         print("Files Moved")
-    else :
-        os.makedirs(file_path + '/' + extention)
-        shutil.move(file_path + '/' + i, file_path + '/' + extention + '/' + i)
+    else:
+        # If the directory doesn't exist, create it
+        os.makedirs(file_path + '/' + extension)
+        # Move the file to the newly created directory with the corresponding extension
+        shutil.move(file_path + '/' + i, file_path + '/' + extension + '/' + i)
         print("Folder created. Files Moved.")
